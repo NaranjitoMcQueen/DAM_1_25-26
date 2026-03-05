505fa13e8f04c79c0ac3c1ef832a5fe176821a0b
Arnau McQueen <166841225+NaranjitoMcQueen@users.noreply.github.com>
2026-03-05 20:33:26 +0100

    Actualizar Concessionari.java

--- Cotxes_Repas/src/vehiclesBase/Concessionari.java
+++ Cotxes_Repas/src/vehiclesBase/Concessionari.java
@@ -65,11 +65,11 @@ public class Concessionari {
 		return mitjana / ConceVeh.size();
 	}
 
-	public boolean eliminarVehicle(Vehicle v) {
+	/*public boolean eliminarVehicle(Vehicle v) {
 	    return ConceVeh.remove(v);
-	}
+	}*/
 	
-	public boolean eliminarVehicle(String matricula) {
+	public boolean eliminarVehicle(String matricula, Vehicle v) {
 	    for (Vehicle v : ConceVeh) {
 	        if (v.getMatricula().equals(matricula)) {
 	            ConceVeh.remove(v);
